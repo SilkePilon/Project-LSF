7f915483d7647918033cb0ea625dcbcf7aca8fdf
Silke pilon <silkepilon2009@gmail.com>
2024-09-16 15:32:55 +0200

    Update menu script to add settings button functionality

--- Assets/Scenes/Scripts/Menu.cs
+++ Assets/Scenes/Scripts/Menu.cs
@@ -1,43 +1,85 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.SceneManagement;
-using System.Runtime.Serialization;
-using System;
+
 public class CameraSwitch : MonoBehaviour
 {
     public Button playButton;
     public Button exitButton;
+    public Button settingsButton;
+    public Animation settingsAnimation;
+    public Animation benchAnimation;
 
-    public
+    private bool isSettingsOpen = false;
 
     void Start()
     {
+        SetupButton(playButton, SwitchToGameScene, "Play");
+        SetupButton(exitButton, ExitGame, "Exit");
+        SetupButton(settingsButton, ToggleSettingsMenu, "Settings");
 
-        // Add a listener to the play button
-        if (playButton != null)
+        CheckAnimationComponent(settingsAnimation, "Settings");
+        CheckAnimationComponent(benchAnimation, "Bench");
+    }
+
+    void SetupButton(Button button, UnityEngine.Events.UnityAction action, string buttonName)
+    {
+        if (button != null)
         {
-            playButton.onClick.AddListener(SwitchToGameScene);
-        }
-        if (exitButton != null)
-        {
-            exitButton.onClick.AddListener(ExitGame);
+            button.onClick.AddListener(action);
         }
         else
         {
-            Debug.LogError("Play button not assigned in the inspector!");
+            Debug.LogWarning($"{buttonName} button not assigned in the inspector!");
+        }
+    }
+
+    void CheckAnimationComponent(Animation anim, string animName)
+    {
+        if (anim == null)
+        {
+            Debug.LogWarning($"{animName} Animation component not assigned in the inspector!");
         }
     }
 
     void SwitchToGameScene()
     {
-        SceneManager.LoadScene(sceneName:"game");
+        SceneManager.LoadScene("game");
     }
 
     void ExitGame()
     {
         Application.Quit();
-        
+    }
+
+    void ToggleSettingsMenu()
+    {
+        isSettingsOpen = !isSettingsOpen;
+
+        if (isSettingsOpen)
+        {
+            PlayAnimation(settingsAnimation, "OpenSettings");
+            PlayAnimation(benchAnimation, "Bench_drop");
+        }
+        else
+        {
+            // Add logic to close the settings menu if needed
+            // For example:
+            // PlayAnimation(settingsAnimation, "settingsDown");
+            // PlayAnimation(benchAnimation, "Bench_rise");
+        }
+    }
+
+    void PlayAnimation(Animation anim, string clipName)
+    {
+        if (anim != null && anim.GetClip(clipName) != null)
+        {
+            anim[clipName].wrapMode = WrapMode.Once;
+            anim.Play(clipName);
+        }
+        else
+        {
+            Debug.LogWarning($"Unable to play animation: {clipName}. Animation or clip is missing.");
+        }
     }
 }
